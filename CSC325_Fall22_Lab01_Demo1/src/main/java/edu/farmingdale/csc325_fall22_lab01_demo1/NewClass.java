7ce6b83b11534d96e74806079d1a64ce17cdffe3
britgw@farmingdale.edu <skysn@137.125.129.211>
2022-09-01 10:06:48 -0400

    change

--- CSC325_Fall22_Lab01_Demo1/src/main/java/edu/farmingdale/csc325_fall22_lab01_demo1/NewClass.java
+++ CSC325_Fall22_Lab01_Demo1/src/main/java/edu/farmingdale/csc325_fall22_lab01_demo1/NewClass.java
@@ -1,13 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package edu.farmingdale.csc325_fall22_lab01_demo1;
 
-/**
- *
- * @author skysn
- */
+
 public class NewClass {
     
+    
+    public static void main(String[] args) {
+    
+    }
 }
