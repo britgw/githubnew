3ec56ab2448073478cf175c43615031b39884313
britgw@farmingdale.edu <>
2022-09-01 09:53:44 -0400

    Init class creation

---
+++ CSC325_Fall22_Lab01_Demo1/src/main/java/edu/farmingdale/csc325_fall22_lab01_demo1/NewClass.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.farmingdale.csc325_fall22_lab01_demo1;
+
+/**
+ *
+ * @author skysn
+ */
+public class NewClass {
+    
+}
